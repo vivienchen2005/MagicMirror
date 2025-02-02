541f9295c5805d492d5daa140ab31424f86e4866
Taisia <tmertz@usc.edu>
2025-02-02 15:13:04 -0800

    changed clothing model from png to svg

--- application/frontend/src/App.tsx
+++ application/frontend/src/App.tsx
@@ -1,26 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-import Model from './components/Model.tsx'
-import femaleSkin from './assets/female_skin.png'
+import { useState } from "react";
+import "./App.css";
+import FemaleModel from "./components/FemaleModel.tsx";
+
 
 function App() {
-  const [color, setColor] = useState("#000000");
 
   return (
     <>
-      <input
-        type="color"
-        value={color}
-        onChange={(e) => setColor(e.target.value)}
-        className="w-16 h-10 border rounded"
-      />
-      <div className="flex justify-center items-center h-screen">
-        {/* <ModelTest></ModelTest> */}
-        <Model src={femaleSkin} color={color} maxWidth={750} maxHeight={750} />
-      </div>
+      <FemaleModel></FemaleModel>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
